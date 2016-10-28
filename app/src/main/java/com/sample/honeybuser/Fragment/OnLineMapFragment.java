94ebff43cc16acc3563eb234aab5f94fbaf9f524
RajaSelvi <raja@imaginetventures.com>
2016-10-28 13:24:45 +0530

    Working with google map

--- app/src/main/java/com/sample/honeybuser/Fragment/OnLineMapFragment.java
+++ app/src/main/java/com/sample/honeybuser/Fragment/OnLineMapFragment.java
@@ -225,6 +225,7 @@ public class OnLineMapFragment extends Fragment {
             }
         }
         ChangeLocationSingleton.getInstance().locationChanges(null, null, adres);
+        Complete.offerDialogInstance().orderCompleted();
     }
 
     private void setVendorDetailsItem(Vendor vendorByMarkerId) {
